88bd3b919e727feebd75ffa9940f97bf01c9fe19
Ryan Broadsword <rbroadsword@gmail.com>
2022-05-19 22:11:15 -0700

    delete all console.logs from Universe methods

--- src/js/universe.js
+++ src/js/universe.js
@@ -12,43 +12,36 @@ export default class Universe {
 
   venusAge() {
       let ageOnVenus = Math.round(this.age / .62); 
-      console.log(ageOnVenus); 
       return ageOnVenus; 
     }
 
     marsAge() {
       let ageOnMars = Math.round(this.age / 1.88); 
-      console.log(ageOnMars); 
       return ageOnMars; 
     }
 
     jupiterAge() {
       let ageOnJupiter = Math.round(this.age / 11.86); 
-      console.log(ageOnJupiter); 
       return ageOnJupiter; 
     }
 
     lifeExpectancyOnMercury() {
       let expectancyOnMercury = Math.round(this.lifeExpectancy / .24); 
-      console.log(expectancyOnMercury); 
       return expectancyOnMercury; 
     }
 
     lifeExpectancyOnVenus() {
       let expectancyOnVenus = Math.round(this.lifeExpectancy / .62); 
-      console.log(expectancyOnVenus); 
       return expectancyOnVenus; 
     }
 
     lifeExpectancyOnMars() {
       let expectancyOnMars = Math.round(this.lifeExpectancy / 1.88); 
-      console.log(expectancyOnMars); 
       return expectancyOnMars; 
     }
 
     lifeExpectancyOnJupiter() {
       let expectancyOnJupiter = Math.round(this.lifeExpectancy / 11.86); 
-      console.log(expectancyOnJupiter); 
       return expectancyOnJupiter; 
     }
   }
