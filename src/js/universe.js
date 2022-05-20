9f0cc8be1f999c1f66e72e6b04ea93b16aa9a584
Ryan Broadsword <rbroadsword@gmail.com>
2022-05-19 20:37:23 -0700

    pass test one for universe constructor

--- src/js/universe.js
+++ src/js/universe.js
@@ -1,5 +1,7 @@
 export default class Universe {
   constructor(age, lifeExpectancy, lifeLeft){
-
+    this.age = age; 
+    this.lifeExpectancy = lifeExpectancy; 
+    this.lifeLeft = lifeExpectancy - age; 
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'template',      // 
+      title: 'space-age',      // 
       template: './src/index.html',
       inject: 'body'
     })
